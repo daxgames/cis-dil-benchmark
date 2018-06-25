325dc3b7e980d3de5ec939b713f3afa9916ae37d
Michael Hoffman <michael.hoffman@ge.com>
2018-06-25 15:18:09 -0400

    updated selinux

--- controls/1_6_mandatory_access_control.rb
+++ controls/1_6_mandatory_access_control.rb
@@ -141,7 +141,7 @@ if cis_level == '2'
     end
 
     processes(/.*/).where { pid > 0 }.entries.each do |p|
-      its(p.property_name) { should_not match /awsagent/ }
+      its(p.process_name) { should_not match /awsagent/ }
       describe p.label.to_s.split(':')[2] do
         it { should_not cmp 'initrc_t' }
       end
